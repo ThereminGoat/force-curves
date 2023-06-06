744f62bf32d503023661ac896db9d992d4830ed2
epikepik <evripidis78@gmail.com>
2023-06-06 15:41:42 +0100

    Finished creating db

--- 0_data_process/data_process.py
+++ 0_data_process/data_process.py
@@ -6,41 +6,40 @@ import pandas as pd
 class DataProcess:
 
     def main(self):
-        """Main program."""
+        """Reads data and creates SQL db with all switch downstroke/upstroke profiles."""
 
+        # Create the SQL table
         self.create_table()
-
+        # Get connection and cursor
         conn, cur = self.create_conn()
 
-        # For each file with data
-        for ii in self.rummage_through()[0:4]:
-
-
-            _dic = self.read_excel_data_file(ii)
-
-            self.add_switch_data_to_table(conn, cur, _dic)
+        # For each file with switch data
+        for idx, ii in enumerate(self.rummage_through()):
+            try:
+                # Get data from switch file
+                data_dic = self.read_excel_data_file(ii)
+            except ValueError as E:
+                print(E)
+            # Add switch data to sql table
+            self.add_switch_data_to_table(conn, data_dic)
 
         conn.commit()
         conn.close()
-        return _dic
-
-    @staticmethod
-    def create_conn():
-        conn = None
-        try:
-            conn = sqlite3.connect('force_curves')
-            return conn, conn.cursor()
-        except Exception as E:
-            print(E)
 
         return
 
-    def add_switch_data_to_table(self, conn, cur, dic):
+    @staticmethod
+    def add_switch_data_to_table(conn, dic):
+        """Append switch data to SQL table.
 
+            Parameters:
+                conn: Connection to SQL table
+                dic: Dictionary with data with keys ['Downstroke', 'Upstroke'] and
+                values dataframe with columns ['force', 'displacement', 'switch_name', 'mode'].
+            """
         for kk, df in dic.items():
+            print('Doing {} for {}'.format(df['switch_name'][0], df['mode'][0]))
             df.to_sql('force_curves', conn, if_exists='append', index=False)
-
-
 
         return
 
@@ -54,27 +53,26 @@ class DataProcess:
                 Returns dictionary with keys ['Downstroke', 'Upstroke'] and values dataframe
                 with columns ['Force', 'Displacement', 'Switch_Name', 'Mode']
         """
-
         # Get name of switch
-        _df = pd.read_excel(file_path, header=None, usecols='B', skiprows=1,
-                            sheet_name=['DataTable'])
-        switch_name = _df['DataTable'].iloc[0, 0]
+        df = pd.read_excel(file_path, header=None, usecols='B', skiprows=1,
+                           sheet_name=['DataTable'])
+        switch_name = df['DataTable'].iloc[0, 0]
 
         # Get dictionary with data from Excel. Keys are Downstroke, Upstroke
         # and columns are 2 (force) and 11 (displacement).
-        _dic = pd.read_excel(file_path, header=None, usecols='C,L',
-                             skiprows=5, sheet_name=['Downstroke', 'Upstroke'])
+        data_dic = pd.read_excel(file_path, header=None, usecols='C,L',
+                                 skiprows=5, sheet_name=['Downstroke', 'Upstroke'])
 
         # Rename columns and add switch name and mode to the df
-        for kk, df in _dic.items():
-            df.rename(columns={2: 'Force', 11: 'Displacement'}, inplace=True)
-            df['switch_name'] = switch_name
-            df['mode'] = kk
+        for kk, _df in data_dic.items():
+            _df.rename(columns={2: 'Force', 11: 'Displacement'}, inplace=True)
+            _df['switch_name'] = switch_name
+            _df['mode'] = kk
 
-        return _dic
+        return data_dic
 
     def create_table(self):
-
+        """Create SQL table."""
         conn, cur = self.create_conn()
 
         cur.execute(""" DROP TABLE IF EXISTS force_curves """)
@@ -90,11 +88,27 @@ class DataProcess:
         cur.close()
         conn.close()
 
+    @staticmethod
+    def create_conn():
+        """Return connection and cursor of the SQL table.
+
+            Parameters:
+            Returns:
+                Tuple with SQL connection and cursor
+        """
+        conn = None
+        try:
+            conn = sqlite3.connect('force_curves')
+            return conn, conn.cursor()
+        except Exception as E:
+            print(E)
+
+        return
+
     def rummage_through(self) -> list[str]:
-        """Return a list with the file paths of all data file.
+        """Return a list with the file paths of all switch data files.
 
         Parameters:
-
         Returns:
             List with file paths of all data files
         """
@@ -108,13 +122,12 @@ class DataProcess:
 
     @staticmethod
     def get_files_name(switch_dir: str) -> list[str]:
-        """Return a list with all the directories of data in the specific switch directory.
+        """Return a list with the file paths of switch data in the specific directory.
 
         Parameters:
             switch_dir: Name of directory for the specific switch
-
         Returns:
-            List with file paths with data in the directory
+            List with file paths with switch data in the directory
         """
         # Find all the files in the dir and create a list with the one that hold the raw data
         # Might be more than one.
@@ -130,9 +143,8 @@ class DataProcess:
     def get_switches_directories() -> list[str]:
         """Return a list with the directories of all switches.
 
-            :param:
-                None
-            :return:
+        Parameters:
+        Returns:
                 List with the directories of all switches
             """
         directory_names = [ii[0] for ii in os.walk('../')]
