922518dc8c8243dd964bfeb6a298cf030deec5df
epikepik <evripidis78@gmail.com>
2023-06-12 13:41:15 +0100

    Finished with comments

--- 0_data_process/data_plot.py
+++ 0_data_process/data_plot.py
@@ -5,6 +5,7 @@ import os
 import pandas as pd
 import matplotlib.pyplot as plt
 import numpy as np
+from typing import List
 from switches_exceptions import *
 reload(sys.modules['switches_exceptions'])
 from switches_exceptions import *
@@ -14,13 +15,18 @@ class DataPlot:
 
     def __init__(self, switches_id_todo=None):
 
+        if switches_id_todo is None:
+            switches_id_todo = [100, 200, 300, 400, 500]
         self.conn, self.cur = self.connect_db()
         self.switch_dic = self.all_switches_dic()
-        if switches_id_todo is None:
-            self.switches_id_todo = self.run_exceptions([100, 200, 300, 400, 500])
+        self.switches_id_todo = self.run_exceptions(switches_id_todo)
 
-    def run_exceptions(self, switches_id_todo):
-
+    def run_exceptions(self, switches_id_todo: List[int]):
+        """Run exceptions on the list with the switches ID.
+            Parameters:
+                switches_id_todo: List of the IDs of the switches to do
+            Returns:
+                switches_id_todo: List of the IDs of the switches to do post-exceptions."""
         while True:
             try:
                 if not all(isinstance(ii, int) for ii in switches_id_todo):
@@ -36,7 +42,10 @@ class DataPlot:
         return switches_id_todo
 
     def plot_switches(self):
-
+        """Do plots.
+            Parameters:
+            Returns:
+                None"""
         fig, ax = plt.subplots(nrows=2, ncols=1, dpi=100, figsize=(8, 12))
         legend_cols = ["#648ace", "#c2843c", "#ab62c0", "#6ca659", "#ca556a"]
 
@@ -49,43 +58,42 @@ class DataPlot:
                 ax[idx0].plot(switch_data[:, 1], switch_data[:, 0], color=legend_cols[idx1],
                               lw=2)
 
-            self.work_figure(ax[idx0], figure_switches_names, mode)
+            self.work_figure(ax[idx0], fig, figure_switches_names, mode)
 
     @staticmethod
-    def work_figure(ax, figure_switches_names, mode):
-        """Works on the figure.
-
+    def work_figure(ax, fig, figure_switches_names, mode):
+        """Works on the figure and axis handle.
             Parameters:
                 ax: Figure axis
+                fig: Figure handle
                 figure_switches_names: List with names of the plotted switches
                 mode: Downstroke/Upstroke
-
             Returns:
-        """
+                None"""
         ax.set_xlabel('Displacement (mm)', fontsize=13)
-        ax.set_ylabel('Force', fontsize=13)
+        ax.set_ylabel('Force (gf)', fontsize=13)
         ax.set_title('Force Curves, {}'.format(mode), fontsize=14)
         ax.set_xlim([-.2, 4])
         ax.set_ylim([0, 200])
+        fig.patch.set_facecolor('gray')
+        fig.patch.set_alpha(.4)
 
         if mode == 'Upstroke':
             ax.invert_xaxis()
         else:
-            ax.legend(figure_switches_names, loc=2)
+            leg = ax.legend(figure_switches_names, loc=2, frameon=True, fontsize=12)
+            leg.get_frame().set_edgecolor('black')
 
         return
 
     def get_switch_data(self, idx: int, mode: str) -> np.ndarray:
         """Return array with force and displacement data for the particular switch
             and mode.
-
             Parameters:
                 idx: switch id
                 mode: 'Downstroke'/'Upstroke'
-
             Return:
-                Array with force and displacement data for the switch to be plotted
-            """
+                Array with force and displacement data for the switch to be plotted."""
         _query = """SELECT force, displacement
                     FROM force_curves
                     WHERE switch_name = ? AND mode = ?"""
@@ -93,16 +101,15 @@ class DataPlot:
         rows = self.cur.fetchall()
         # Array in db is string, convert to float
         arr = np.array(np.asarray(rows), dtype=float)
+
         return arr
 
     def all_switches_dic(self) -> dict[int, str]:
-        """Return dictionary with keys an id and values the name of the 
-            switch for all switches.
-
+        """Return dictionary with keys an id and values the name of the switch
+            for all switches.
             Parameters:
             Returns:
-                Dictionary with keys the index, and values the name of the switch
-            """
+                Dictionary with keys the index, and values the name of the switch."""
         switch_names = self.get_all_switch_names()
         # Index for the switches
         idx = list(range(1, len(switch_names) + 1))
@@ -110,8 +117,11 @@ class DataPlot:
 
         return dic
 
-    def get_all_switch_names(self):
-
+    def get_all_switch_names(self) -> List[str]:
+        """Return all switch names in the db.
+            Parameters:
+            Returns:
+                List with all switch names in the list."""
         _query = 'SELECT DISTINCT(switch_name) FROM force_curves'
         df = pd.read_sql_query(_query, self.conn)
 
@@ -122,7 +132,6 @@ class DataPlot:
     @staticmethod
     def connect_db():
         """Return connection and cursor of the SQL table.
-
             Parameters:
             Returns:
                 Tuple with SQL connection and cursor
--- 0_data_process/switches_exceptions.py
+++ 0_data_process/switches_exceptions.py
@@ -1,4 +1,8 @@
+from typing import List
+
+
 class SwitchesExceptions(Exception):
+    """Exception parent class."""
 
     @staticmethod
     def correction(switches_to_do, **kwargs):
@@ -6,27 +10,41 @@ class SwitchesExceptions(Exception):
 
 
 class SwitchIdNotInt(SwitchesExceptions):
-
+    """Exception class for when any of the input switch ID is not int."""
     @staticmethod
-    def correction(switches_to_do, **kwargs):
+    def correction(switches_to_do: List[int], **kwargs) -> List[int]:
+        """Return list with default switch IDs when input list is not all integers.
+            Parameters:
+                switches_to_do: Input switches
+            Returns:
+                List with default switches ID"""
         print('Inputs not all integers, reverting to defaults')
         return [100, 200, 300, 400, 500]
 
 
 class SwitchIdOutsideRange(SwitchesExceptions):
-
+    """Exception for when an input switch ID is outside range"""
     @staticmethod
     def correction(switches_to_do, **kwargs):
+        """Return updated switch ID list
+            Parameters:
+                switches_to_do: Input switches
+            Returns:
+                List with updated switches ID"""
         print('Switches ID outside range, removing offending IDs')
-
         switches_to_do = [ii for ii in switches_to_do if ii >= kwargs['min']]
         switches_to_do = [ii for ii in switches_to_do if ii <= kwargs['max']]
         return switches_to_do
 
 
 class NumberOfSwitches(SwitchesExceptions):
-
+    """Exception for when input list with switch IDs has length greater than 5."""
     @staticmethod
     def correction(switches_to_do, **kwargs):
+        """Return updated switch ID list
+            Parameters:
+                switches_to_do: Input switches
+            Returns:
+                Truncated list of length 5"""
         print('Only 5 switch allowed for plotting, truncating...')
         return switches_to_do[:5]
